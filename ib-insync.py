423d5263e6e6513558ffe417f6001d862037dad9
Andres Aguilar <aguilarcarboni@gmail.com>
2024-12-16 12:42:46 -0600

    Update order

--- ib-insync.py
+++ ib-insync.py
@@ -17,14 +17,6 @@ from utils.api import access_api
 import pandas as pd
 import io
 
-ib = IB()
-ib.connect('127.0.0.1', 4001, clientId=1)
-if ib.isConnected():
-    logger.info('Connected')
-else:
-    logger.error('Not connected')
-    raise Exception('Not connected')
-
 response = access_api('/drive/download_file', method='POST', data={'file_id': '1_dvlQLjb5kAw45mMNO0PrugOoohcgF_G', 'mime_type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'})
 
 try:
@@ -34,6 +26,15 @@ try:
 except Exception as e:
     logger.error(f"Error parsing Excel file: {str(e)}")
     raise Exception(f"Error parsing Excel file: {str(e)}")
+
+
+ib = IB()
+ib.connect('127.0.0.1', 4001, clientId=1)
+if ib.isConnected():
+    logger.info('Connected')
+else:
+    logger.error('Not connected')
+    raise Exception('Not connected')
 
 """
 bond = Bond(secIdType='ISIN', secId='US620076AM16')
