5f169d7852fe15d7f7cacca1983a9bf8dd70c281
Andres Aguilar <aguilarcarboni@gmail.com>
2024-12-16 14:09:24 -0600

    Update ib-insync.py

--- ib-insync.py
+++ ib-insync.py
@@ -18,13 +18,13 @@ isin_codes = df['ISIN'].str.strip().tolist()
 ib = IB()
 
 try:
-    ib.connect('127.0.0.1', 4001, clientId=1)
+    ib.connect('172.17.0.1', 4001, clientId=1)
 except Exception as e:
     logger.error(f"Error connecting to IB: {str(e)}")
     raise Exception(f"Error connecting to IB: {str(e)}")
 
 bonds = []
-logger.info(f'Found a total of {len(isin_codes)} bonds in the database, fetching details...')
+logger.info(f'Found a total of {len(isin_codes)} bonds in the database, fetching details for the first 5...')
 
 for isin in isin_codes[0:5]:
     bond = Bond(secIdType='ISIN', secId=isin)
