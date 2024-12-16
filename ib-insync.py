630bd06de3417bb4a2ceca51afa5389928acc1da
Andres Aguilar <aguilarcarboni@gmail.com>
2024-12-16 11:44:16 -0600

    Update script

--- ib-insync.py
+++ ib-insync.py
@@ -2,5 +2,14 @@ from ib_insync import *
 
 ib = IB()
 ib.connect('127.0.0.1', 4001, clientId=1)
-print('Connected?: ', ib.isConnected())
+if ib.isConnected():
+    print('Connected')
+else:
+    raise Exception('Not connected')
+
+amd = Stock('AMD')
+details = ib.reqContractDetails(amd)
+contracts = [cd.contract for cd in details]
+print(contracts)
+
 ib.disconnect()
