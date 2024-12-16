a4800531ef88fd2aab6b48925faca6487b9e4bdb
Andres Aguilar <aguilarcarboni@gmail.com>
2024-12-16 12:43:46 -0600

    Update so it reads csv

--- ib-insync.py
+++ ib-insync.py
@@ -1,27 +1,14 @@
 from ib_insync import *
 from utils.logger import logger
 from utils.api import access_api
-
-ib = IB()
-ib.connect('127.0.0.1', 4001, clientId=1)
-if ib.isConnected():
-    logger.info('Connected')
-else:
-    logger.error('Not connected')
-    raise Exception('Not connected')
-
-
-from ib_insync import *
-from utils.logger import logger
-from utils.api import access_api
 import pandas as pd
 import io
 
 response = access_api('/drive/download_file', method='POST', data={'file_id': '1_dvlQLjb5kAw45mMNO0PrugOoohcgF_G', 'mime_type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'})
 
 try:
-    excel_data = io.BytesIO(response)
-    df = pd.read_excel(excel_data)
+    csv_data = io.BytesIO(response)
+    df = pd.read_csv(csv_data)
     logger.info(df.head())
 except Exception as e:
     logger.error(f"Error parsing Excel file: {str(e)}")
